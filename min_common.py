b343dc3aff95f687896d1a54ae7a2585412d08e4
Ted Yu <o23958078@gmail.com>
2024-10-02 17:53:27 +0800

    loop times decide

--- min_common.py
+++ min_common.py
@@ -1,14 +1,30 @@
-a = [1, 2, 3, 4, 5, 6]
-b = [9, 6, 4, 19, 7]
-ans = []
+def s_into_i(st):
+    ints = []
+    if isinstance(st, str):
+        for s in st:
+            ints.append(s)
+        return ints
+    else:
+        return st
 
 
 def min_common(l1, l2):
+    ans_mc = []
     for n in l1:
         if n in l2:
-            ans.append(n)
-    ans.sort()
-    return ans[0]
+            ans_mc.append(n)
+    ans_mc.sort()
+    return ans_mc[0]
 
 
-print(min_common(a, b))
+def main(inp1, inp2):
+    inp1 = s_into_i(a)
+    inp2 = s_into_i(b)
+    ans_m = min_common(inp1, inp2)
+    return ans_m
+
+
+a = [1, 2, 3, 4, 5, 6]
+b = [9, 6, 4, 19, 7]
+
+print(main(a, b))
