50eafcdb757b97b9d850cf5eeb56ff353953535a
Ted Yu <o23958078@gmail.com>
2024-10-02 16:53:01 +0800

    into function

--- min_common.py
+++ min_common.py
@@ -1,9 +1,14 @@
 a = [1, 2, 3, 4, 5, 6]
 b = [9, 6, 4, 19, 7]
-ans =[]
-for n in a:
-    if n in b:
-        ans.append(n)
-ans.sort()
+ans = []
 
-print(ans[0])
+
+def min_common(l1, l2):
+    for n in l1:
+        if n in l2:
+            ans.append(n)
+    ans.sort()
+    return ans[0]
+
+
+print(min_common(a, b))
